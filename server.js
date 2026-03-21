ce55b309dceea70b664de5e987a02a042c4fa4db
MAD_MAX <164201950+Bhargav63788@users.noreply.github.com>
2026-03-21 15:46:32 +0530

    Initialize Express server with location endpoints
    Set up Express server with CORS and JSON parsing. Added endpoints to update and retrieve bus location.

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
+const express = require("express");
+const cors = require("cors");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+let busLocation = { lat: 0, lng: 0 };
+
+// receive location
+app.post("/update-location", (req, res) => {
+  busLocation = req.body;
+  console.log("Updated:", busLocation);
+  res.send("Location updated");
+});
+
+// send location
+app.get("/get-location", (req, res) => {
+  res.json(busLocation);
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
